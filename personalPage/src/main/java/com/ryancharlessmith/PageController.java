c069d1e0b71d21d716407321f20b910d6becd0c8
RyCSmith <rycsmith@gmail.com>
2016-03-21 23:01:45 -0400

    removed nonsense js, moved into spring mvc

---
+++ personalPage/src/main/java/com/ryancharlessmith/PageController.java
@@ -0,0 +1,16 @@
+package com.ryancharlessmith;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String greeting(Model model) {
+        return "index";
+    }
+
+}
