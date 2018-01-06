4fcc7d2b98eb16013180c552d07ab887397f78f3
RyCSmith <rycsmith@gmail.com>
2018-01-06 14:04:00 -0500

    add constructor and id

--- react/site/src/client/app/project/ProjectsSingleView.jsx
+++ react/site/src/client/app/project/ProjectsSingleView.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class ProjectsSingleView extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		// add state like // this.state = {date: new Date()};
+	}
 
-  render() {
-    return (
-		<div>
-			<div>ProjectsSingleView</div>
-			<li><Link to='/test/projects'>Back to Projects</Link></li>
-	    </div>
-	);
-  }
+	render() {
+		return (
+			<div>
+				<div>ProjectsSingleView</div>
+				<div>{this.props.match.params.number}</div>
+				<li><Link to='/test/projects'>Back to Projects</Link></li>
+			</div>
+		);
+	}
 
 }
 
