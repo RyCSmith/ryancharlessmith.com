c4e96613261df23193d774f582f7a8c900f83905
RyCSmith <rycsmith@gmail.com>
2018-01-06 22:23:08 -0500

    finished navbar v1

--- react/site/src/client/app/NavBar.jsx
+++ react/site/src/client/app/NavBar.jsx
@@ -7,28 +7,22 @@ let NavBar = (props) => {
 		<div className="nav-bar">
 		  	<div className={classNames("container", "nav-bar-container", "text-uppercase")}>
 		  		<div className="row">
-					<div className={classNames("col-lg-6")}>
-						<div className={classNames("row", "nav-name-box")}>
-							<div className={classNames("col-lg-6")}>
-								<div className={classNames("nav-name", "text-uppercase", "text-right")}>Ryan C Smith</div>
-							</div>
-							<div className={classNames("col-lg-6")}>
-								<div className={classNames("nav-title")}>Software Developer</div>
-							</div>
-						</div>
+					<div className={classNames("col-lg-6", "nav-name-box")}>
+						<div className={classNames("nav-name", "text-uppercase")}>Ryan C Smith</div>
+						<div className={classNames("nav-title")}>Software Developer</div>
 					</div>
 					<div className="col-lg-6">
-						<div className={classNames("row", "links-row", "text-right")}>
-							<div className={classNames("col-sm-3", "link-col")}>
+						<div className={classNames("row", "links-row")}>
+							<div className={classNames("col", "link-col")}>
 								<Link to='/test'>About</Link>
 							</div>
-							<div className={classNames("col-sm-3", "link-col")}>
+							<div className={classNames("col", "link-col")}>
 								<Link to='/test/projects'>Experience</Link>
 							</div>
-							<div className={classNames("col-sm-3", "link-col")}>
+							<div className={classNames("col", "link-col")}>
 								<Link to='/test/projects'>Projects</Link>
 							</div>
-							<div className={classNames("col-sm-3", "link-col")}>
+							<div className={classNames("col", "link-col")}>
 								<Link to='/test/projects'>Contact</Link>
 							</div>
 						</div>
